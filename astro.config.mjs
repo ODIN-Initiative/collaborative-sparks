e7f5235d473894e68ae580575b6660a84c79bdda
Newman <nslanier@yahoo.com>
2024-11-08 09:59:06 +0800

    adding base: to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import starlightBlog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://odin-initiative.github.io/collaborative-sparks-website',
+	site: 'https://odin-initiative.github.io/',
+	base: 'collaborative-sparks-website',
+	trailingSlash: "never",
 	integrations: [
 		starlight({
 			plugins: [starlightBlog()],
