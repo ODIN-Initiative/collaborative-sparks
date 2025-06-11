58def2cbda4098a7483db0ccc002ea620868bd7c
Randall <randall@inator.biz>
2025-06-11 12:17:41 -0700

    add obsidian-starlight plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,58 +1,64 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 //import starlightBlog from 'starlight-blog';
 import tailwindcss from "@tailwindcss/vite";
-
+import starlightObsidian, { obsidianSidebarGroup } from "starlight-obsidian";
+import { watchObsidianVault } from "./vaultWatcher.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://odin-initiative.github.io/collaborative-sparks-website/',
-	base: '/collaborative-sparks-website',
-	trailingSlash: "always",
-	//vite: {    plugins: [],  },
-	vite: {    plugins: [tailwindcss()],  },
-	integrations: [
-		starlight({
-			//plugins: [starlightBlog()],
-			title: 'Collaborative Sparks',
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/CollaborativeSparks'},
-				{ icon: 'external', label: 'ODIN', href: 'https://www.odin.eco/'}
-			   
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'sparks' },
-				},
-				{
-					label: 'Blog',
-					autogenerate: { directory: 'blog' },
-				},
-				{
-					label: 'Patterns',
-					autogenerate: { directory: 'patterns' },
-				},
-				{ 
-					label: 'Links',
-					items: [ 
-						{
-							label: 'ODIN website',
-							link: 'https://odin.eco'
-						},
+    site: "https://odin-initiative.github.io/collaborative-sparks-website/",
+    // base: "/collaborative-sparks-website",
+    // trailingSlash: "always",
+    //vite: {    plugins: [],  },
+    vite: { plugins: [tailwindcss()] },
+    integrations: [
+        watchObsidianVault("./authoring"),
+        starlight({
+            //plugins: [starlightBlog()],
+            title: "Collaborative Sparks",
+            social: [
+                { icon: "github", label: "GitHub", href: "https://github.com/CollaborativeSparks" },
+                { icon: "external", label: "ODIN", href: "https://www.odin.eco/" },
+            ],
+            plugins: [
+                starlightObsidian({
+                    vault: "./authoring",
+                    output: "o",
+                    copyFrontmatter: "all",
+                    // ignore: ["Blog"],
+                    sidebar: {
+                        label: "Guides",
+                        collapsed: false,
+                        collapsedFolders: false,
+                    }
+                }),
+            ],
+            sidebar: [
+                {
+                    label: "Latest News",
+                    autogenerate: { directory: "sparks" },
+                },
+                {
+                    label: "Blog",
+                    autogenerate: { directory: "content/docs/o/_Blog" },
+                },
+                obsidianSidebarGroup,
+                {
+                    label: "Links",
+                    items: [
+                        {
+                            label: "ODIN website",
+                            link: "https://odin.eco",
+                        },
+                    ],
+                },
+                //{ label: 'Resource Article Blog', link: '/blog/' },
+            ],
+            customCss: [],
+        }),
+    ],
 
-					]
-				},
-				//{ label: 'Resource Article Blog', link: '/blog/' },
-			],
-			customCss: [],
-		}),
-
-	],
-
-	output: 'static',  // Ensure it builds static assets
+    output: "static", // Ensure it builds static assets
 });
-
-
-
